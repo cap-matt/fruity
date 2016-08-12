bc4f0c8dc588d56faa3f9ea82f51d69adf61da9b
Matt Seymour <mattseyjunk@hotmail.com>
2016-08-12 02:24:45 +0100

    refactor to handle multiple items

--- main/src/fruity/FruitCart.java
+++ main/src/fruity/FruitCart.java
@@ -1,30 +1,52 @@
 package fruity;
 
+import java.util.List;
+
 /**
- * A simple Fruit Shop 'checkout' system for calculating the total cost of a given 'item'.
+ * A simple Fruit Shop 'checkout' system for calculating the total cost of a given set of
+ * 'items'.
+ * <p>
+ * The main method of interest to client code is the getTotal() method.
  *
  * @author Matt Seymour
  */
 public class FruitCart {
+    public static String APPLE = "Apple";
+    public static String ORANGE = "Orange";
+
     /**
-     * Get the cost of the given item.
+     * Get the total cost of the given items.
      *
-     * @param item The cost of the given item.
-     * @return The cost of the given item.
+     * @param items The items to be 'totalled'.
+     * @return The total cost of the given items.
      */
-    public static int getTotal(String item) {
-        int total = 0;
+    public static int getTotal(List<String> items) {
+        final int apples = count(APPLE, items);
+        final int oranges = count(ORANGE, items);
 
-        if ("Apple".equalsIgnoreCase(item)) {
-            total = 60;
+        return (apples * 60) + (oranges * 25);
+    }
+
+    /**
+     * Count the number of occurrences of the given <tt>item</tt> within the
+     * given <tt>items</tt> List.
+     *
+     * @param item  The item name being sought.
+     * @param items The List in which to 'search' for the given item name.
+     * @return The number of occurrences of the given <tt>item</tt> within the
+     * given <tt>items</tt> List.
+     */
+    public static int count(String item, List<String> items) {
+        int count = 0;
+
+        if (item != null && items != null) {
+            for (String currItem : items) {
+                if (item.equalsIgnoreCase(currItem)) {
+                    count++;
+                }
+            }
         }
 
-        if ("Orange".equalsIgnoreCase(item)) {
-            total = 25;
-        }
-
-        //assumption: no error handling necessary of 'invalid' item names
-
-        return total;
+        return count;
     }
 }
